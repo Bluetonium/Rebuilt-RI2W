4573a01d7b535b2fdf8117ba3abc396afbba68b0
Kaiden <132237036+CapableDrake@users.noreply.github.com>
2026-09-08 17:37:19 -0500

    Finished base Intake Subsystem
    Intake subsystem should have the necessities to do PID tuning and testing.
    
    Like Shooter, no actual commands have been created.
    
    Care should be taken with the intake extender, as that motor should be limited in its movement and could break the intake. I don't know how to deal with that yet

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,19 +1,27 @@
 package frc.robot.subsystems.intake;
 
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.SignalLogger;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 public class Intake extends SubsystemBase {
 
     private TalonFX m_intakeMotor;
     private TalonFX m_intakeExtendMotor;
 
-    private final VoltageOut m_intakeMotor_sysIdControl = new VoltageOut(0);
-    private final VoltageOut m_intakeExtendMotor_sysIdControl = new VoltageOut(0);
+    private final VoltageOut m_intakeMotor_SysIdControl = new VoltageOut(0);
+    private final VoltageOut m_intakeExtendMotor_SysIdControl = new VoltageOut(0);
 
     private TalonFXConfiguration m_intakeMotorConfig;
     private TalonFXConfiguration m_intakeExtendMotorConfig;
@@ -24,4 +32,96 @@ public class Intake extends SubsystemBase {
     private MotionMagicVelocityVoltage m_intakeExtendMotorVelocityVoltage = new MotionMagicVelocityVoltage(0)
             .withAcceleration(IntakeConstants.INTAKE_EXTEND_MOTOR.ACCELERATION);
 
+    private final SysIdRoutine m_intakeMotor_SysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(
+            null,
+            Volts.of(4),
+            null,
+            (state) -> SignalLogger.writeString("SysIdIntake", state.toString())),
+            new SysIdRoutine.Mechanism(
+                    (volts) -> m_intakeMotor.setControl(
+                            m_intakeMotor_SysIdControl.withOutput(volts.in(Volts))),
+                    null,
+                    this));
+
+    private final SysIdRoutine m_intakeExtendMotor_SysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(
+            null,
+            Volts.of(4),
+            null,
+            (state) -> SignalLogger.writeString("SysIdIntakeExtender", state.toString())),
+            new SysIdRoutine.Mechanism(
+                    (volts) -> m_intakeExtendMotor.setControl(
+                            m_intakeExtendMotor_SysIdControl.withOutput(volts.in(Volts))),
+                    null,
+                    this));
+
+    public Intake() {
+        m_intakeMotor = new TalonFX(IntakeConstants.INTAKE_MOTOR.ID);
+        m_intakeMotor.setNeutralMode(IntakeConstants.INTAKE_MOTOR.NEUTRAL_MODE);
+        m_intakeMotorConfig = new TalonFXConfiguration();
+        m_intakeMotorConfig.MotorOutput.Inverted = IntakeConstants.INTAKE_MOTOR.INVERTED_VALUE;
+        m_intakeMotorConfig.CurrentLimits = IntakeConstants.INTAKE_MOTOR.CURRENT_LIMITS;
+
+        Slot0Configs slot0_intake = m_intakeMotorConfig.Slot0;
+        slot0_intake.kP = IntakeConstants.INTAKE_MOTOR.kP;
+        slot0_intake.kI = IntakeConstants.INTAKE_MOTOR.kI;
+        slot0_intake.kD = IntakeConstants.INTAKE_MOTOR.kD;
+        slot0_intake.kS = IntakeConstants.INTAKE_MOTOR.kS;
+        slot0_intake.kV = IntakeConstants.INTAKE_MOTOR.kV;
+        slot0_intake.kA = IntakeConstants.INTAKE_MOTOR.kA;
+
+        // ------------------------Intake Extender------------------------------
+        m_intakeExtendMotor = new TalonFX(IntakeConstants.INTAKE_EXTEND_MOTOR.ID);
+        m_intakeExtendMotor.setNeutralMode(IntakeConstants.INTAKE_EXTEND_MOTOR.NEUTRAL_MODE);
+        m_intakeExtendMotorConfig = new TalonFXConfiguration();
+        m_intakeExtendMotorConfig.MotorOutput.Inverted = IntakeConstants.INTAKE_EXTEND_MOTOR.INVERTED_VALUE;
+        m_intakeExtendMotorConfig.CurrentLimits = IntakeConstants.INTAKE_EXTEND_MOTOR.CURRENT_LIMITS;
+
+        Slot0Configs slot0_intakeExtend = m_intakeExtendMotorConfig.Slot0;
+        slot0_intakeExtend.kP = IntakeConstants.INTAKE_EXTEND_MOTOR.kP;
+        slot0_intakeExtend.kI = IntakeConstants.INTAKE_EXTEND_MOTOR.kI;
+        slot0_intakeExtend.kD = IntakeConstants.INTAKE_EXTEND_MOTOR.kD;
+        slot0_intakeExtend.kS = IntakeConstants.INTAKE_EXTEND_MOTOR.kS;
+        slot0_intakeExtend.kV = IntakeConstants.INTAKE_EXTEND_MOTOR.kV;
+        slot0_intakeExtend.kA = IntakeConstants.INTAKE_EXTEND_MOTOR.kA;
+
+        applyConfig();
+    }
+
+    public void setup() {
+        setDefaultCommand(run(() -> {
+            m_intakeMotor.setControl(m_intakeMotorVelocityVoltage.withVelocity(0));
+            m_intakeExtendMotor.setControl(m_intakeExtendMotorVelocityVoltage.withVelocity(0));
+        }).withName("Intake Subsystem Stopped"));
+
+        IntakeStates.setupStates();
+    }
+
+    public void applyConfig() {
+        StatusCode intake_status = m_intakeMotor.getConfigurator().apply(m_intakeMotorConfig);
+        StatusCode intakeExtend_status = m_intakeExtendMotor.getConfigurator().apply(m_intakeExtendMotorConfig);
+
+        if (!intake_status.isOK())
+            DriverStation.reportWarning(intake_status.getName() + "Failed to apply configs to intake"
+                    + intake_status.getDescription(), false);
+        if (!intakeExtend_status.isOK())
+            DriverStation.reportWarning(intakeExtend_status.getName() + "Failed to apply configs to intake extender"
+                    + intakeExtend_status.getDescription(), false);
+    }
+
+    // PID Commands
+    public Command m_intakeMotor_SysIdQuasistatic(SysIdRoutine.Direction direction) {
+        return m_intakeMotor_SysIdRoutine.quasistatic(direction);
+    }
+
+    public Command m_intakeMotor_SysIdDynamic(SysIdRoutine.Direction direction) {
+        return m_intakeMotor_SysIdRoutine.dynamic(direction);
+    }
+
+    public Command m_intakeExtendMotor_SysIdQuasistatic(SysIdRoutine.Direction direction) {
+        return m_intakeExtendMotor_SysIdRoutine.quasistatic(direction);
+    }
+
+    public Command m_intakeExtendMotor_SysIdDynamic(SysIdRoutine.Direction direction) {
+        return m_intakeExtendMotor_SysIdRoutine.dynamic(direction);
+    }
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeStates.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeStates.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems.intake;
 
-public class IntakeStates {
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.RobotContainer;
+import frc.robot.RobotStates;
 
+public class IntakeStates {
+    private static Intake intake = RobotContainer.getIntake();
+
+    public static void setupStates() {
+        RobotStates.sysDyn.whileTrue(intake.m_intakeMotor_SysIdDynamic(Direction.kForward));
+        RobotStates.sysSta.whileTrue(intake.m_intakeMotor_SysIdDynamic(Direction.kForward));
+        RobotStates.sysDynRev.whileTrue(intake.m_intakeMotor_SysIdDynamic(Direction.kReverse));
+        RobotStates.sysStaRev.whileTrue(intake.m_intakeMotor_SysIdDynamic(Direction.kReverse));
+
+        RobotStates.sysDyn.whileTrue(intake.m_intakeExtendMotor_SysIdDynamic(Direction.kForward));
+        RobotStates.sysSta.whileTrue(intake.m_intakeExtendMotor_SysIdDynamic(Direction.kForward));
+        RobotStates.sysDynRev.whileTrue(intake.m_intakeExtendMotor_SysIdDynamic(Direction.kReverse));
+        RobotStates.sysStaRev.whileTrue(intake.m_intakeExtendMotor_SysIdDynamic(Direction.kReverse));
+
+    }
 }
